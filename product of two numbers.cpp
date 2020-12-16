c4fe98173bc677d8bf3b7e3553c8eacec2dd2e65
annavaram <annavaramdasari99@gmail.com>
2020-12-16 04:33:00 -0800

    created product of two numbers.cpp
    print product of two numbers in cpp

---
+++ product of two numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+   double firstNumber, secondNumber, productoftwoNumbers;
+   cout << "Enter two numbers: ";
+   cin >> firstNumber >> secondNumber;
+   product of two numbers = firstNumber * secondNumber;
+   cout << "product = " << productoftwoNumbers;
+   
+   return 0;
+   
+   
+}
